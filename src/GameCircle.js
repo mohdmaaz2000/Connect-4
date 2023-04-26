721f487ca46e36ee2c0d9711bc3abb9c35dc4437
Mohd Maaz <97888220+mohdmaaz2000@users.noreply.github.com>
2023-04-27 01:01:08 +0530

    Square To Circle Component

--- src/GameCircle.js
+++ src/GameCircle.js
@@ -4,9 +4,17 @@ const onClickEvent = (e,id,children) =>{
 }
 const GameCircle = (props) => {
     const {id,children,color} = props;
+    const style = {
+        backgroundColor:color,
+        margin:10,
+        width:75,
+        height:75,
+        borderRadius:'50%',
+        border:'2px solid black'
+    }
   return (
     // if we want to pass the event we simply do it like this 
-    <div style={{backgroundColor:color}} onClick={(e)=> {onClickEvent(e,id,children)}}> 
+    <div style={style} onClick={(e)=> {onClickEvent(e,id,children)}}> 
       {children}
     </div>
   )
--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -2,31 +2,50 @@ import React from 'react'
 import GameCircle from '../GameCircle'
 
 export default function GameBoard() {
+    const style = {
+        display:'grid',
+        gridTemplateColumns:'1fr 1fr 1fr 1fr',
+        gridTemplateRows:'1fr 1fr 1fr 1fr',
+        padding:10,
+        width :500,
+        position:'absolute',
+        left:'50%',
+        marginLeft:'-250px',
+        top:'20%'
+    }
     return (
-        <div>
+        <div style={style}>
             <GameCircle id={1} color='blue'>
-               <span style={{color:'red'}}>Red</span>
             </GameCircle>
             <GameCircle id={2} color='red'>
-            <span style={{color:'blue'}}>Blue</span>
             </GameCircle>
             <GameCircle id={3} color='blue'>
-            <span style={{color:'red'}}>Red</span>
             </GameCircle>
             <GameCircle id={4} color='red'>
-            <span style={{color:'blue'}}>Blue</span>
             </GameCircle>
             <GameCircle id={5} color='blue'>
-            <span style={{color:'red'}}>Red</span>
             </GameCircle>
             <GameCircle id={6} color='red'>
-            <span style={{color:'blue'}}>Blue</span>
             </GameCircle>
             <GameCircle id={7} color='blue'>
-            <span style={{color:'red'}}>Red</span>
             </GameCircle>
             <GameCircle id={8} color='red'>
-            <span style={{color:'blue'}}>Blue</span>
+            </GameCircle>
+            <GameCircle id={9} color='blue'>
+            </GameCircle>
+            <GameCircle id={10} color='red'>
+            </GameCircle>
+            <GameCircle id={11} color='blue'>
+            </GameCircle>
+            <GameCircle id={12} color='red'>
+            </GameCircle>
+            <GameCircle id={13} color='blue'>
+            </GameCircle>
+            <GameCircle id={14} color='red'>
+            </GameCircle>
+            <GameCircle id={15} color='blue'>
+            </GameCircle>
+            <GameCircle id={16} color='red'>
             </GameCircle>
         </div>
     )
