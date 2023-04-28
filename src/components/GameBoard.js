3ade87fb9c4d8e4a963d51f94b8c7572e6b3291e
Mohd Maaz <97888220+mohdmaaz2000@users.noreply.github.com>
2023-04-28 12:04:44 +0530

    intitialise the game board and add more functions

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,49 +1,44 @@
 import React, { useState } from 'react'
 import GameCircle from '../GameCircle'
 
-export default function GameBoard() {   
-    const [board,setBoard] = useState(Array(16).fill(0));
-    const circleClicked = (id) =>
-    {
-        console.log('Cirlce Clicked : '+id);
-        board[id - 1] = 1;
-        setBoard(board);
+const PLAYER_0 = 0;
+const NO_CIRCLES = 16;
+const PLAYER_1 = 1;
+const PLAYER_2 = 2;
+export default function GameBoard() {
+    const [board, setBoard] = useState(Array(16).fill(PLAYER_0));
+    const [playerState, setPlayerState] = useState(PLAYER_1);
+
+    const circleClicked = (id) => {
+        console.log('Cirlce Clicked : ' + id);
+        
+        setBoard(prev=>{
+            return prev.map((circle,pos) => {
+                if(pos === id) return playerState;
+                return circle;
+            });
+        });
+        setPlayerState(playerState === PLAYER_1 ? PLAYER_2 : PLAYER_1);
         console.log(board);
+        console.log(playerState);
+    }
+
+    const renderAllCircle = () =>
+    {
+        let circles = [];
+        for(let i = 0; i < NO_CIRCLES;i++)
+        {
+            circles.push(renderCircle(i));
+        }
+        return circles;
+    }
+    const renderCircle = id =>{
+        return <GameCircle id={id} className={`player_${board[id]}`} circleClicked={circleClicked}/>
     }
     return (
         <div className='gameBoard'>
-            <GameCircle id={1} className='player_0' circleClicked={circleClicked}>
-            </GameCircle>
-            <GameCircle id={2} className='player_1' circleClicked={circleClicked}>
-            </GameCircle>
-            <GameCircle id={3} className='player_2' circleClicked={circleClicked}>
-            </GameCircle>
-            <GameCircle id={4} circleClicked={circleClicked}>
-            </GameCircle>
-            <GameCircle id={5} circleClicked={circleClicked}>
-            </GameCircle>
-            <GameCircle id={6} circleClicked={circleClicked}>
-            </GameCircle>
-            <GameCircle id={7} circleClicked={circleClicked}>
-            </GameCircle>
-            <GameCircle id={8} circleClicked={circleClicked}>
-            </GameCircle>
-            <GameCircle id={9} circleClicked={circleClicked}>
-            </GameCircle>
-            <GameCircle id={10} circleClicked={circleClicked}>
-            </GameCircle>
-            <GameCircle id={11} circleClicked={circleClicked}>
-            </GameCircle>
-            <GameCircle id={12} circleClicked={circleClicked}>
-            </GameCircle>
-            <GameCircle id={13} circleClicked={circleClicked}>
-            </GameCircle>
-            <GameCircle id={14} circleClicked={circleClicked}>
-            </GameCircle>
-            <GameCircle id={15} circleClicked={circleClicked}>
-            </GameCircle>
-            <GameCircle id={16} circleClicked={circleClicked}>
-            </GameCircle>
+            {renderAllCircle()}
+        
         </div>
     )
 }
